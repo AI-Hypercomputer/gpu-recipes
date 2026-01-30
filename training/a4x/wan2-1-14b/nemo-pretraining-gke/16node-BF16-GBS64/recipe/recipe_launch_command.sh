045fe8e81b4ca05332e7db213b2d9eafb3865200
ngu3 <ninggu@google.com>
2026-01-30 00:04:36 +0000

    add wan 2.1 14b gke 16 node recipe

---
+++ training/a4x/wan2-1-14b/nemo-pretraining-gke/16node-BF16-GBS64/recipe/recipe_launch_command.sh
@@ -0,0 +1,1 @@
+helm install ninggu-wan21 . -f values.yaml --set-file workload_launcher=launcher.sh --set workload.image=nvcr.io/nvidia/nemo:25.11.00 --set volumes.gcsMounts[0].bucketName=ubench-logs --set volumes.gcsMounts[0].mountPath=/job-logs --set workload.envs[0].value=/job-logs/ninggu-ubench-wan21
