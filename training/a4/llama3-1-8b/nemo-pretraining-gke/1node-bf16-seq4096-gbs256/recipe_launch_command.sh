b182d558a6b2829875b38e4fe9e716cebd6fbd54
Vishwas Reddy <vishwasreddy38@gmail.com>
2026-03-19 19:54:19 +0000

    feat: add 1-node bf16 recipe for llama3-1-8b

---
+++ training/a4/llama3-1-8b/nemo-pretraining-gke/1node-bf16-seq4096-gbs256/recipe_launch_command.sh
@@ -0,0 +1,1 @@
+helm install vishwasreddy-ubench-d5hr . -f values.yaml --set-file workload_launcher=launcher.sh --set-file workload_config=llama3-1-8b-bf16-seq4096-gbs256-gpus8.py --set workload.image=nvcr.io/nvidia/nemo:25.11 --set volumes.gcsMounts[0].bucketName=ubench-logs --set volumes.gcsMounts[0].mountPath=/job-logs --set workload.envs[0].value=/job-logs/vishwasreddy-ubench-d5hr
