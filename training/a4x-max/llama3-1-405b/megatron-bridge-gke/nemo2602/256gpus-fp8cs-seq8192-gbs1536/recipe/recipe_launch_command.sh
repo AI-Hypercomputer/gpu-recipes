a2bb22a121702cbd25d057809d2f8ac0fe2ecbe4
chihchuanchan <chihchuanchan@google.com>
2026-04-24 21:46:33 +0000

    Remove unused files

--- training/a4x-max/llama3-1-405b/megatron-bridge-gke/nemo2602/256gpus-fp8cs-seq8192-gbs1536/recipe/recipe_launch_command.sh
+++
@@ -1,5 +0,0 @@
-helm install $USER-a4x-max-llama3-1-405b-256gpus . -f values.yaml \
-  --set-file workload_launcher=launcher.sh \
-  --set-file workload_config=custom_setup_experiment.py \
-  --set workload.image=nvcr.io/nvidia/nemo:26.02 \
-  --set queue=tas-lq
